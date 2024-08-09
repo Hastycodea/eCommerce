6458f20cad4e7b2c072492ad459caee9fc5516a9
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-09 09:48:11 +0300

    loadIntoH2

---
+++ ecom/src/main/java/com/hastycode/ecom/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.hastycode.ecom.controller;
+
+import com.hastycode.ecom.model.Product;
+import com.hastycode.ecom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Good Morning Kings and Queens";
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProduct() {
+        return service.getAllProducts();
+    }
+
+}
