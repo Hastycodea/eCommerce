828d5515f0911053275bee96889cb1727d9a8ad8
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-10 12:06:59 +0300

    fetching image

--- ecom/src/main/java/com/hastycode/ecom/controller/ProductController.java
+++ ecom/src/main/java/com/hastycode/ecom/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.hastycode.ecom.model.Product;
 import com.hastycode.ecom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,6 +47,14 @@ public class ProductController {
         } catch (Exception e) {
             return  new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/product/{id}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
     }
 
 }
